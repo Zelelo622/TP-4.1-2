563985c7e5a7ec7bf0403b22b61b40a0479978e4
Diana2503 <dbesxmelnicyna@mail.ru>
2023-04-02 20:43:04 +0300

    get post запросы для категорий

--- grocery-store/server/routes/index.js
+++ grocery-store/server/routes/index.js
@@ -1,8 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const userRouter = require('./userRouter')
+const categoryRouter = require('./categoryRouter')
 
-router.use('/user', userRouter)
+router.use('/user', userRouter);
+router.use('/category', categoryRouter);
 
 module.exports = router
 
