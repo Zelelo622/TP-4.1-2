36780a93c8737db8f933a193bebe12d974836e98
Egor Kirin <egorcom27@gmail.com>
2023-04-13 23:38:54 +0300

    начало верстки страницы товара

--- grocery-store/client/src/components/product/ProductDetails.js
+++ grocery-store/client/src/components/product/ProductDetails.js
@@ -1,10 +1,36 @@
 import React from "react";
+import "../../assets/styles/ProductDetails.css";
 
 const ProductDetails = ({ product }) => {
   return (
-    <div>
-      <h2>{product.name}</h2>
-      <p>{product.price}</p>
+    <div className="product-details-container">
+      <div className="product-image">
+        <img src={product.img} alt={product.name} />
+      </div>
+      <div className="product-info">
+        <h2 className="product-title">{product.name}</h2>
+        <div className="product-cart">
+          <p className="product-price">{product.price} руб.</p>
+          <button className="add-to-cart-btn">В корзину</button>
+        </div>
+        <div className="product-description">
+          <div className="product-comp">
+            <p className="comp-title">Состав:</p>
+            <p className="comp-text">{product.composition}</p>
+          </div>
+          <div className="product-nutrition">
+            <p className="nutrition-title">Пищевая ценность на 100г:</p>
+            <ul className="nutrition-list">
+              <li className="nutrition-item">Белки: {product.protein} г</li>
+              <li className="nutrition-item">Жиры: {product.fat} г</li>
+              <li className="nutrition-item">
+                Углеводы: {product.carbohydrates} г
+              </li>
+              <li className="nutrition-item">Ккал: {product.calories} ккал</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- grocery-store/client/src/pages/Product.js
+++ grocery-store/client/src/pages/Product.js
@@ -1,23 +1,32 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Context } from "../index";
+import React, { useEffect, useState } from "react";
 import { fetchOneProduct } from "../http/productAPI";
 import { useParams } from "react-router-dom";
-import { observer } from "mobx-react-lite";
 import ProductDetails from "../components/product/ProductDetails";
+import Header from "../components/Header";
+import { Container } from "react-bootstrap";
+import Footer from "../components/Footer";
 
-const Product = observer(() => {
-  const [product, setProduct] = useState();
+const Product = () => {
+  const [product, setProduct] = useState({});
   const { name } = useParams();
 
   useEffect(() => {
-    fetchOneProduct(name).then(data => setProduct(data))
+    fetchOneProduct(name).then((data) => setProduct(data));
   }, []);
 
   return (
-    <div>
-      <ProductDetails product={product} />
-    </div>
+    <>
+      <div className="container-page">
+        <Header />
+        <main className="main">
+          <Container>
+            <ProductDetails product={product} />
+          </Container>
+        </main>
+        <Footer />
+      </div>
+    </>
   );
-});
+};
 
 export default Product;
