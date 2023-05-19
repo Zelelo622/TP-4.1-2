213e2744273091b884406d3d003390584979ed6d
Diana2503 <dbesxmelnicyna@mail.ru>
2023-05-19 09:50:04 +0300

    обновлен метод getAll и добавлено удаление пользователей для админов

--- grocery-store/server/routes/profileRouter.js
+++ grocery-store/server/routes/profileRouter.js
@@ -7,5 +7,6 @@ router.get("/couriers", userController.getAllCourier);
 router.get("/:phone", userController.getOne);
 router.put("/:phone", userController.update);
 router.delete("/:phone", userController.delete);
+router.delete("/:phone/admin", userController.deleteUserForAdmin);
 
 module.exports = router;
