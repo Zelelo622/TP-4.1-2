19cf2d584f6f59c11c2bc73ac1c6a4f3172b5444
Egor Kirin <egorcom27@gmail.com>
2023-05-19 16:55:31 +0300

    обновлена пагинация

--- grocery-store/client/src/components/product/PagesProduct.js
+++ grocery-store/client/src/components/product/PagesProduct.js
@@ -1,7 +1,7 @@
 import { observer } from "mobx-react-lite";
 import React, { useContext } from "react";
 import { Context } from "../..";
-import { Pagination } from "react-bootstrap";
+import Pagination from "react-bootstrap/Pagination";
 
 const PagesProduct = observer(() => {
   const { product } = useContext(Context);
@@ -12,18 +12,68 @@ const PagesProduct = observer(() => {
     pages.push(i + 1);
   }
 
+  const visiblePages = 7;
+  const ellipsisThreshold = Math.floor(visiblePages / 2);
+
+  const handlePageChange = (page) => {
+    product.setPage(page);
+    window.scrollTo(0, 0);
+  };
+
+  const getPageRange = () => {
+    if (pageCount <= visiblePages) {
+      return pages;
+    }
+    const startPage = Math.max(
+      Math.min(product.page - ellipsisThreshold, pageCount - visiblePages + 1),
+      1
+    );
+    const endPage = Math.min(startPage + visiblePages - 1, pageCount);
+    return pages.slice(startPage - 1, endPage);
+  };
+
   return (
     <div>
       <Pagination className="mt-5">
-        {pages.map((page) => (
-          <Pagination.Item
-            key={page}
-            active={product.page === page}
-            onClick={() => product.setPage(page)}
-          >
-            {page}
-          </Pagination.Item>
-        ))}
+        <Pagination.First onClick={() => handlePageChange(1)} />
+        <Pagination.Prev
+          onClick={() => handlePageChange(product.page - 1)}
+          disabled={product.page === 1}
+        />
+
+        {getPageRange().map((page) => {
+          if (
+            page === 1 ||
+            page === pageCount ||
+            page === product.page ||
+            (page >= product.page - ellipsisThreshold &&
+              page <= product.page + ellipsisThreshold)
+          ) {
+            return (
+              <Pagination.Item
+                key={page}
+                active={product.page === page}
+                onClick={() => handlePageChange(page)}
+              >
+                {page}
+              </Pagination.Item>
+            );
+          } else if (
+            (page === product.page - ellipsisThreshold - 1 &&
+              product.page > ellipsisThreshold + 2) ||
+            (page === product.page + ellipsisThreshold + 1 &&
+              product.page < pageCount - ellipsisThreshold - 1)
+          ) {
+            return <Pagination.Ellipsis key={`ellipsis${page}`} />;
+          }
+          return null;
+        })}
+
+        <Pagination.Next
+          onClick={() => handlePageChange(product.page + 1)}
+          disabled={product.page === pageCount}
+        />
+        <Pagination.Last onClick={() => handlePageChange(pageCount)} />
       </Pagination>
     </div>
   );
